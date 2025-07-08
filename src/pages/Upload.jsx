4f96ebb8960c98bf6b259f0085dce927d28253d8
Vedant Kankate <98166840+Vedant2402@users.noreply.github.com>
2025-07-07 18:27:30 -0700

    fixes

---
+++ src/pages/Upload.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhotoUpload from '../components/PhotoUpload';
+
+const Upload = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <PhotoUpload />
+    </div>
+  );
+};
+
+export default Upload;
